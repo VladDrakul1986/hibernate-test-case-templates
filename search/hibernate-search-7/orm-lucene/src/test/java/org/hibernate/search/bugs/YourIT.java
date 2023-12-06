a911a2a68d7716b1e3de486f6083bc94909d4699
Vlad Drakul <grafvlad.drakul@gmail.com>
2023-12-06 16:04:36 +0100

    create Tests with unexpected behavior.

--- search/hibernate-search-7/orm-lucene/src/test/java/org/hibernate/search/bugs/YourIT.java
+++ search/hibernate-search-7/orm-lucene/src/test/java/org/hibernate/search/bugs/YourIT.java
@@ -1,14 +1,16 @@
 package org.hibernate.search.bugs;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
+import static org.hibernate.search.util.common.impl.CollectionHelper.asSet;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.search.engine.search.query.SearchResult;
 import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.mapping.SearchMapping;
 import org.hibernate.search.mapper.orm.session.SearchSession;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class YourIT extends SearchTestBase {
@@ -18,11 +20,12 @@ public class YourIT extends SearchTestBase {
 		return new Class<?>[] { YourAnnotatedEntity.class };
 	}
 
+	// This works as expected. We fill the database from Scratch and the different tenants are handled correctly without mass indexing
 	@Test
 	public void testYourBug() {
+		String myLocalTenantName = "mainTenant";
+		TenantContext.setCurrentTenant(myLocalTenantName);
 		try ( Session s = getSessionFactory().openSession() ) {
-			String myLocalTenantName = "test123";
-			TenantContext.setCurrentTenant(myLocalTenantName);
 			YourAnnotatedEntity yourEntity1 = new YourAnnotatedEntity( 1L, "Jane Smith", myLocalTenantName );
 			YourAnnotatedEntity yourEntity2 = new YourAnnotatedEntity( 2L, "John Doe", myLocalTenantName );
 
@@ -31,18 +34,74 @@ public class YourIT extends SearchTestBase {
 			s.persist( yourEntity2 );
 			tx.commit();
 		}
+		try ( Session session = getSessionFactory().openSession() ) {
+			SearchSession searchSession = Search.session( session );
+
+			SearchResult<YourAnnotatedEntity> searchResult = searchSession.search( YourAnnotatedEntity.class )
+					.where( f -> f.match().field( "name" ).matching( "smith" ) )
+					.fetch(5);
+			Assertions.assertEquals(1, searchResult.total().hitCount());
+			assertThat( searchResult.hits() )
+					.hasSize( 1 )
+					.element( 0 ).extracting( YourAnnotatedEntity::getId )
+					.isEqualTo( 1L );
+		}
+		TenantContext.setCurrentTenant("anotherTenant");
+		try ( Session session = getSessionFactory().openSession() ) {
+			SearchSession searchSession = Search.session( session );
+
+			SearchResult<YourAnnotatedEntity> searchResult = searchSession.search( YourAnnotatedEntity.class )
+					.where( f -> f.match().field( "name" ).matching( "smith" ) )
+					.fetch(5);
+
+			assertThat( searchResult.hits() )
+					.hasSize( 0 );
+			Assertions.assertEquals(0, searchResult.total().hitCount());
+		}
+	}
+
+	// This works NOT as expected. We fill the database from Scratch and perform a mass index on that database.
+	// The different tenant information provided in the database are ignored by the mass indexer which results in a false hitcount
+	// This leads to differences between the expected hitcount and the fetched entities from our database
+	@Test
+	public void testYourBugMassIndexer() throws InterruptedException {
+		String myLocalTenantName = "mainTenant";
+		TenantContext.setCurrentTenant(myLocalTenantName);
+		try ( Session s = getSessionFactory().openSession() ) {
+			YourAnnotatedEntity yourEntity1 = new YourAnnotatedEntity( 1L, "Jane Smith", myLocalTenantName );
+			YourAnnotatedEntity yourEntity2 = new YourAnnotatedEntity( 2L, "John Doe", myLocalTenantName );
+
+			Transaction tx = s.beginTransaction();
+			s.persist( yourEntity1 );
+			s.persist( yourEntity2 );
+			tx.commit();
+		}
+		SearchMapping searchMapping = Search.mapping( getSessionFactory() );
+		searchMapping.scope( Object.class ).massIndexer( asSet( "mainTenant", "anotherTenant" )).purgeAllOnStart(true).startAndWait();
 
 		try ( Session session = getSessionFactory().openSession() ) {
 			SearchSession searchSession = Search.session( session );
 
-			List<YourAnnotatedEntity> hits = searchSession.search( YourAnnotatedEntity.class )
+			SearchResult<YourAnnotatedEntity> searchResult = searchSession.search( YourAnnotatedEntity.class )
 					.where( f -> f.match().field( "name" ).matching( "smith" ) )
-					.fetchHits( 20 );
-
-			assertThat( hits )
+					.fetch(5);
+			Assertions.assertEquals(1, searchResult.total().hitCount());
+			assertThat( searchResult.hits() )
 					.hasSize( 1 )
 					.element( 0 ).extracting( YourAnnotatedEntity::getId )
 					.isEqualTo( 1L );
+		}
+		TenantContext.setCurrentTenant("anotherTenant");
+		try ( Session session = getSessionFactory().openSession() ) {
+			SearchSession searchSession = Search.session( session );
+
+			SearchResult<YourAnnotatedEntity> searchResult = searchSession.search( YourAnnotatedEntity.class )
+					.where( f -> f.match().field( "name" ).matching( "smith" ) )
+					.fetch(5);
+
+			assertThat( searchResult.hits() )
+					.hasSize( 0 );
+			Assertions.assertEquals(0, searchResult.total().hitCount());
 		}
 	}
 
